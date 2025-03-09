fded7d664bf18bbaa337cef9b063d0d99b9502e1
Fabiola Li Wu <fabiliwu@hotmail.com>
2025-03-09 17:35:26 -0400

    Add files via upload

---
+++ h1.cc
@@ -0,0 +1,23 @@
+// Program 1 - HW3
+// Author: <YOUR NAME>
+
+#include "image.h"
+#include <iostream>
+
+using namespace ComputerVisionProjects;
+
+
+int main(int argc, char **argv){
+    if (argc != 3) {
+      std::cout << "Usage: " <<
+        argv[0] << " {input_image_name} {output_edge_image_name}" << std::endl;
+        return 0;
+    }
+
+    const std::string input_file(argv[1]);
+    const std::string output_file(argv[2]);
+
+    std::cout << "Running h1 " << input_file << " " << output_file << std::endl;
+
+    return 0;
+}

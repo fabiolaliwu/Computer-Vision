5bc24938e44c0cc4bd5d22a9402164d17741053e
Fabiola Li Wu <fabiolalw@users.noreply.github.com>
2025-04-06 17:27:49 -0400

    started s3

--- hw4/s3.cc
+++ hw4/s3.cc
@@ -4,7 +4,7 @@
 #include "image.h"
 #include <string>
 #include <iostream>
-
+#include <fstream>
 using namespace ComputerVisionProjects;
 
 int main(int argc, char **argv) {
@@ -23,6 +23,40 @@ int main(int argc, char **argv) {
     const int threshold = atoi(argv[6]);
     const std::string normals_output_file(argv[7]);
     const std::string albedo_output_file(argv[8]);
+
+    Image image1, image2, image3;
+    if (ReadImage(image1_file, &image1) == 0 ||ReadImage(image2_file, &image2) == 0 || ReadImage(image3_file, &image3) == 0) {
+        std::cerr << "Error reading image." << std::endl;
+        return 1;
+    }
+    // read directions
+    std::ifstream directions_file_stream(directions_file);
+    if (!directions_file_stream.is_open()) {
+        std::cerr << "Error opening directions file: " << directions_file << std::endl;
+        return 1;
+    }
+    std::vector<std::vector<double>> directions;
+    while(directions_file_stream){
+        std::vector<double> direction;
+        double x, y, z;
+        directions_file_stream >> x >> y >> z;
+        if(directions_file_stream.eof())
+            break;
+        direction.push_back(x);
+        direction.push_back(y);
+        direction.push_back(z);
+        directions.push_back(direction);
+    }
+    directions_file_stream.close();
+  // for(int r = 0; r < directions.size(); r++){
+  //   for(int c = 0; c < directions[r].size(); c++){
+  //     std::cout << directions[r][c] << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+
+   
+
 
     std::cout << "Running s3: " << directions_file << " " << image1_file <<  " " << image2_file << " " << image3_file
               << " " << step << " " << threshold << " " << normals_output_file << " "
